1dda91f362928c1c4345c58b444e5ce71c853eaf
CamiPalmaS <capalmasoto@gmail.com>
2024-08-19 23:09:31 -0400

    se agrega funcion para actualizar select en formulario con las categorias obtenidas con la funcion getCategorias

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,25 @@ async function getCategories(){
     alert("Hubo un erro: "+error)
     })
 }
+//llamar a la funcion asincrona
+getCategories();
 
+//funcion para mostrar categorias obtenidas en el select para el usuario
 const selectCategories = (categorias) => {
+    categorias.sort((a,b) => a.strCategory.localeCompare(b.strCategory));
     
+    //apuntar al select por ID
+    const selectElement = document.getElementById("categorias");
+
+    //para que recorra las categorias sobre las categorías
+    categorias.forEach(categoria => {
+        //para crear los elementos option para el select
+        const option = document.createElement("option");
+        //asignar valor (id) y titulo/nombre (strCategory)
+        option.value = categoria.idCategory;
+        option.textContent = categoria.strCategory;
+
+        //para que se añadan las opciones
+        selectElement.appendChild(option);
+    });
 }
